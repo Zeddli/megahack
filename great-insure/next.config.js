9f7cf69640c5ed6c0bcc6aaee50195867fb793ef
Zhijian <ruj8320@gmail.com>
2025-05-16 09:08:42 +0800

    update

--- great-insure/next.config.js
+++ great-insure/next.config.js
@@ -11,14 +11,14 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: true,
-        path: true,
-        os: true,
-        crypto: true,
-        stream: true,
-        http: true,
-        https: true,
-        zlib: true
+        fs: false,
+        path: false,
+        os: false,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        zlib: require.resolve('browserify-zlib')
       };
     }
     return config;
